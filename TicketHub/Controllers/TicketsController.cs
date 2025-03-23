b1abbbabfc3c42b3eb25145fcd1bfa0f1e7ef1b8
NSCCDylan <W0443022@nscc.ca>
2025-03-23 15:08:45 -0300

    Add project files.

---
+++ TicketHub/Controllers/TicketsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly ILogger<TicketsController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public TicketsController(ILogger<TicketsController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Welcome to TicketHub!");
+        }
+
+
+
+
+    }
+}
